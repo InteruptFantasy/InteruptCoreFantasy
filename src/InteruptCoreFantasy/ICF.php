1dec62142fe4bca8f571fe54a8dd5a7129fcd6b6
NeuroBind Project <axicsnetwork@gmail.com>
2016-11-26 23:14:04 +0800

    Create ICF.php

---
+++ src/InteruptCoreFantasy/ICF.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace InteruptCoreFantasy;
+
+class ICF extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener {
+
+const GAME_MASTER = "GM";
+const DEFAULT = "";
+
+public $player;
+private $skills;
+
+public function onEnable() {
+
+}
+
+}
